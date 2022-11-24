ec74fac4bfe1bfc20616563c06156ec225a1a6c4
CrossStackQ <crossstack@gmail.com>
2022-11-24 19:07:41 +0530

    qw1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,7 +18,7 @@ const Layout = () => {
         <main className="w-screen overflow-hidden overflow-y-scroll">
           <Outlet />
         </main>
-        <footer>Made with ❤️</footer>
+        <footer>Made with ❤️ By</footer>
       </ThemeProvider>
     </div>
   );
